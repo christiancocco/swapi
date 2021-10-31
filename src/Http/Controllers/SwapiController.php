5e3c8dbba074389a5e619f2fd7caf33110ac81b9
christiancocco <info@christiancocco.com>
2021-10-31 17:35:22 +0100

    Update SwapiController.php

--- src/Http/Controllers/SwapiController.php
+++ src/Http/Controllers/SwapiController.php
@@ -15,9 +15,38 @@ class SwapiController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $results = People::paginate(10);
+        $query = "";
+        if ($request->has('query')) {
+            $query = $request->get('query');
+        };
+        $sort = "";
+        if ($request->has('sort')) {
+            $sort = $request->get('sort');
+        };
+        $sortVer = "ASC";
+        if ($request->has('sortVer')) {
+            $sortVer = $request->get('sortVer');
+        };
+        $itemPerPage = 10;
+        if ($request->has('numitem')) {
+            $itemPerPage = $request->get('numitem');
+        };
+
+        $results = People::when($query != "null" && $query != '', function($q) use($query) {
+            $q->where("name", "like", '%' . $query . '%');
+            $q->orWhere("hair_color", "like", '%' . $query . '%');
+            $q->orWhere("eye_color", "like", '%' . $query . '%');
+            $q->orWhere("skin_color", "like", '%' . $query . '%');
+            $q->orWhere("mass", "like", '%' . $query . '%');
+            $q->orWhere("height", "like", '%' . $query . '%');
+            $q->orWhere("gender", "like", '%' . $query . '%');
+        })
+        ->when($sort != "null" && $sort != '', function($q) use($sort, $sortVer) {
+            $q->orderBy($sort, $sortVer);
+        })
+        ->paginate($itemPerPage);
         return $results;
     }
 
